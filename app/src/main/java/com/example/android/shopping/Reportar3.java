25664b7541886cc6c3e14d46a0696e6ab0369dfd
Alberto <mariue.varela@gmail.com>
2015-06-04 12:57:59 -0300

    Falta revisar. Cambio de pantallas finalizado.

--- app/src/main/java/com/example/android/shopping/Reportar3.java
+++ app/src/main/java/com/example/android/shopping/Reportar3.java
@@ -1,36 +1,75 @@
 package com.example.android.shopping;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class Reportar3 extends ActionBarActivity {
+public class Reportar3 extends ActionBarActivity
+{
+    EditText editText3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportar3);
+
+        final Button boton = (Button) findViewById(R.id.button25);
+        boton.setOnClickListener (new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "Incidente enviado", Toast.LENGTH_SHORT).show();
+                Intent cambiar = new Intent(Reportar3.this, Inicio.class);
+                startActivity(cambiar);
+            }
+        });
+
+        final Button boton2 = (Button) findViewById(R.id.button24);
+        boton2.setOnClickListener (new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent cambiar = new Intent(Reportar3.this, Reportar2.class);
+                startActivity(cambiar);
+            }
+        });
+
+        if(editText3 != null)
+        {
+            boton2.isClickable();
+        }
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_reportar3, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
